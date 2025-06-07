b09303fdd9dccbd224714e590fba4b965feb9355
H <38281581+MHuiG@users.noreply.github.com>
2025-06-07 11:49:38 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
+import numpy as np
+import sympy as sym
+
+PI = np.pi
+z = sym.Symbol('z')
+Phi_0 = sym.cos(0.5 * sym.pi * (z ** 2) + 3 * sym.pi / 8) / sym.cos(sym.pi * z)
+Phi_1 = (1 / (12 * sym.pi * sym.pi)) * sym.diff(Phi_0, z, 3)
+
+
+
+
 
 f = open("./results/test.md", 'w')
 f.write("|  No.   | Zero  |\n|  ----  | ----  |\n")
