a367709caf72dc8b55d33bf21b1858b79fa8e0bd
H <38281581+MHuiG@users.noreply.github.com>
2025-06-09 07:43:50 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ import sympy as sym
 from scipy.special import gamma, comb
 import time
 
-ACC=5
-ACCURACY = 1e-5
+ACC=20
+ACCURACY = 1e-20
 ERROR = 1e-5
-CHANGE_METHOD = 200 # 200 0 -1
+CHANGE_METHOD = -1 # 200 0 -1
 ZeroNo = 0
 ZeroHigh = 0
 ZeroHighAdd = 1000 #1000
